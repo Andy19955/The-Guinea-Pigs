27b75109975298deeae49714e429f9ee511f10b7
Andy19955 <andy_19955@hotmail.com>
2024-08-02 01:09:11 +0200

    Add await for async function

--- js/handlers/handleLatestPostsDisplay.js
+++ js/handlers/handleLatestPostsDisplay.js
@@ -5,7 +5,7 @@ import { displayMessage } from "../ui/shared/displayMessage.js";
 export async function handleLatestPostsDisplay() {
   try {
     const posts = await fetchPosts(1, 10);
-    displayLatestPosts(posts);
+    await displayLatestPosts(posts);
   } catch (error) {
     displayMessage("#message-container-carousel", `Oops, something didn't work as we planned. Error: ${error.message}`, "error");
   }
