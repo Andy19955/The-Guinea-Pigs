0b0163af44311429b231605109101cf1e67295d4
Andy19955 <andy_19955@hotmail.com>
2024-08-02 01:08:21 +0200

    Remove try/catch

--- js/ui/posts/createLatestPost.js
+++ js/ui/posts/createLatestPost.js
@@ -16,13 +16,9 @@ export async function createLatestPost(post) {
 
   const carouselCardImage = document.createElement("img");
   if (post.featured_media !== 0) {
-    try {
-      const featuredMedia = await fetchFeaturedMedia(post.featured_media);
-      carouselCardImage.setAttribute("src", featuredMedia.media_details.sizes.medium_large.source_url);
-      carouselCardImage.setAttribute("alt", featuredMedia.alt_text);
-    } catch (error) {
-      displayMessage("#message-container-carousel", `Oops, something didn't work as we planned. Error: ${error.message}`, "error");
-    }
+    const featuredMedia = await fetchFeaturedMedia(post.featured_media);
+    carouselCardImage.setAttribute("src", featuredMedia.media_details.sizes.medium_large.source_url);
+    carouselCardImage.setAttribute("alt", featuredMedia.alt_text);
   } else {
     carouselCardImage.setAttribute("src", "/images/no-image-found.jpg");
     carouselCardImage.setAttribute("alt", "Two guinea pigs on a pillow with text: Oops, we didn't find a picture...");
