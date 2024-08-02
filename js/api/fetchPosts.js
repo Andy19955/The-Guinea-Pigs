8a0260fb1430eedb9e904eb453ab72d9d6d383c1
Andy19955 <andy_19955@hotmail.com>
2024-08-02 17:37:31 +0200

    Add debugging

--- js/api/fetchPosts.js
+++ js/api/fetchPosts.js
@@ -4,6 +4,8 @@ export async function fetchPosts(page, postsPerPage) {
   const response = await fetch(`${postsUrl}?page=${page}&per_page=${postsPerPage}`);
   if (response.ok) {
     const results = await response.json();
+    console.log(response);
+    console.log(results);
     return results;
   }
 
--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,2 @@
-export const postsUrl = "https://projectexam1.andreasthune.no/wp-json/wp/v2/posts/";
-export const mediaUrl = "https://projectexam1.andreasthune.no/wp-json/wp/v2/media/";
+export const postsUrl = "https://cors.noroff.dev/https://projectexam1.andreasthune.no/wp-json/wp/v2/posts/";
+export const mediaUrl = "https://cors.noroff.dev/https://projectexam1.andreasthune.no/wp-json/wp/v2/media/";
