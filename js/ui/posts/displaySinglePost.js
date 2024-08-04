53e0d91913d24b368320ab8f54cf3aaa3b007be9
Andy19955 <andy_19955@hotmail.com>
2024-08-04 13:33:41 +0200

    Remove unused import

--- js/ui/posts/displaySinglePost.js
+++ js/ui/posts/displaySinglePost.js
@@ -1,6 +1,5 @@
 import { createSinglePost } from "./createSinglePost.js";
 import { addPostImageEventListener } from "../../ui/posts/addPostImageEventListener.js";
-import { fetchFeaturedMedia } from "../../api/fetchFeaturedMedia.js";
 
 export async function displaySinglePost(post) {
   const postContainer = document.querySelector("#post-container");
